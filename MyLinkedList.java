cb44ff1dee8c33564aefc23add7add5ed1e9e6d7
MelissaH <iheartumedicdroid@gmail.com>
2024-04-04 10:19:53 -0700

    feat: add initial linkedList functionality

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,6 @@
 public class MyLinkedList<T> {
-    Node<T> head = null;
+    private Node<T> head = null;
+    private int size = 0;
 
     public void AddFront(T data) {
         Node<T> newNode = new Node<T>(data);
@@ -9,6 +10,33 @@ public class MyLinkedList<T> {
             newNode.next = head;
             head = newNode;
         }
+        size++;
+    }
+
+    /**
+     * Adds a new node to the end of the LinkedList
+     */
+    public void Append(T data) {
+        Node<T> currentNode = head;
+
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
+
+        // once we have our current last node
+        Node<T> newNode = new Node<T>(data); // create the new node and ensure it's next is null
+        currentNode.next = newNode; // set the next of the old last node equal to the new node
+        size++;
+    }
+    
+    // 1
+    public void Insert(int index) {
+        Node<T> currentNode = head;
+        int currentIndex = 0;
+
+        for (int i = 0; i < index; i++){
+            
+        }
     }
 
     @Override
@@ -17,9 +45,14 @@ public class MyLinkedList<T> {
         String returnString = "";
 
         while (currentNode != null) {
-            returnString += currentNode.data.toString();
+            returnString += " " + currentNode.data.toString();
+            currentNode = currentNode.next;
         }
-        
+
         return returnString;
+    }
+    
+    public int GetSize() {
+        return size;
     }
 }
--- Program.java
+++ Program.java
@@ -9,7 +9,12 @@ public class Program {
     public static void main(String[] args) {
         MyLinkedList<String> myLinkedList = new MyLinkedList<String>();
         myLinkedList.AddFront("World!");
+        // Add a new Node here
         myLinkedList.AddFront("Hello");
+        myLinkedList.AddFront("This is me");
+        myLinkedList.AddFront("Hi");
+        myLinkedList.Append("Dog");
+        System.out.println(myLinkedList.GetSize());
         System.out.println(myLinkedList);
     }
     
